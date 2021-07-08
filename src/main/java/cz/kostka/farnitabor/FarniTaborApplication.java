aa62afa30e02844371560efb253f3e5bdab3ba7d
David Kostka <dkostka@eurofunk.com>
2021-07-08 14:42:16 +0200

    Initial commit

---
+++ src/main/java/cz/kostka/farnitabor/FarniTaborApplication.java
@@ -0,0 +1,13 @@
+package cz.kostka.farnitabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarniTaborApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarniTaborApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/kostka/farnitabor/FarniTaborApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.kostka.farnitabor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarniTaborApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
